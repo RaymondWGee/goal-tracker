6a4c64a78451a6542f249e19ef94f4433f39cc45
RaymondWGee <RaymondWGee@gmail.com>
2022-06-01 15:26:31 -0700

    added routes structure, static string response atm

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,12 @@ const express = require('express')
 // for environment variables
 const dotenv = require('dotenv').config()
 // port being started on
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 // initializing express
 const app = express()
+
+// directs to goalRoutes
+app.use('/api/goals', require('./routes/goalRoutes'))
 
 // takes in port, and a function
 app.listen(port, () => console.log(`server started on port ${port}`))
