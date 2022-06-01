ac34049b49c321399a2ea1740c2c96146310ebfb
RaymondWGee <RaymondWGee@gmail.com>
2022-06-01 15:11:27 -0700

    basic express & env file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('hello server')
+// back end framework
+const express = require('express')
+// for environment variables
+const dotenv = require('dotenv').config()
+// port being started on
+const port = process.env.PORT || 4000
+// initializing express
+const app = express()
+
+// takes in port, and a function
+app.listen(port, () => console.log(`server started on port ${port}`))
