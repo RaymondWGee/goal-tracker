d29dc4afa605be4f85c27377da1da667b695474b
RaymondWGee <RaymondWGee@gmail.com>
2022-06-02 22:10:12 -0700

    added other routes for creating, updating, deleting goal

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,8 +2,24 @@
 const express = require('express')
 const router = express.Router()
 
+// retrieve goal
 router.get('/', (req, res) => {
     res.status(200).json({ message: 'Get Goals' })
+})
+
+// create goal
+router.post('/', (req, res) => {
+    res.status(200).json({ message: 'Create Goals' })
+})
+
+// update goal
+router.put('/:id', (req, res) => {
+    res.status(200).json({ message: `Update Goals ${req.params.id}` })
+})
+
+// delete goal
+router.delete('/', (req, res) => {
+    res.status(200).json({ message: `Delete Goals ${req.params.id}` })
 })
 
 module.exports = router
